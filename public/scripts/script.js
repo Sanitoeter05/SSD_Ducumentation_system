8ef6e540aec1a85f01dc9b81b0e0dca540cdb992
Sanitoeter05 <Niklas.joosten05@gmail.com>
2023-06-29 14:56:09 +0200

    added functional dashboard; fixed login

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,3 +13,20 @@ fetch("/data/patient?count=1")
     .then((result) => {
         document.getElementById("patients").innerHTML = result[0].count;
     });
+
+fetch("/get_current_user_inf")
+    .then((response) => response.json())
+    .then((resul) => {
+        let username = resul.u_name;
+        if (username) {
+            document.getElementById(
+                "log/reg"
+            ).innerHTML = `<a class="navbar-brand mb-0 h1" href="user.html">${username}</a>`;
+            let sanis = document.getElementById("sanis-image-b")
+            sanis.setAttribute("class", "card-img-top img-fluid bg-info")
+        } else {
+            document.getElementById(
+                "log/reg"
+            ).innerHTML = `<a class="navbar-brand mb-0 h1" href="login.html">login!</a>`;
+        }
+    });
